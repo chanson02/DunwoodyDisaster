386a49f665dd1fe38e161e3b9cdd89147608a9fd
Cooper Hanson <cooperhanson2020@gmail.com>
2024-03-08 10:59:16 -0600

    Got a main window to appear

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+The entry point for the game
+"""
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
