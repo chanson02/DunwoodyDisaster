70a554d77389b46317fb59aa8452b553dbff5793
Cooper Hanson <cooperhanson2020@gmail.com>
2024-03-08 11:22:01 -0600

    Implemented a basic fight screen

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ The entry point for the game
 
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from views.fight import FightScreen
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Pokemon-like Game')
+        self.setGeometry(100, 100, 800, 600)
+
+        self.setCentralWidget(FightScreen())
 
 
 if __name__ == '__main__':
---
+++ views/fight.py
@@ -0,0 +1,21 @@
+from PySide6.QtWidgets import QWidget, QPushButton, QGridLayout
+
+
+class FightScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.attack_button = QPushButton('Attack')
+        self.defend_button = QPushButton('Defend')
+
+        self.lay_out = self.create_layout()
+        self.setLayout(self.lay_out)
+
+    def create_layout(self) -> QGridLayout:
+        layout = QGridLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        layout.addWidget(self.attack_button, 0, 0)
+        layout.addWidget(self.defend_button, 0, 1)
+        return layout
