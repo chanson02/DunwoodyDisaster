056917b0433f7defa85d6704ee0c825c731e9628
Cooper Hanson <cooperhanson2020@gmail.com>
2024-03-24 19:00:31 -0500

    Linted

--- CharacterFactory.py
+++ CharacterFactory.py
@@ -1,3 +1,55 @@
+class Character:
+    def __init__(self):
+        # Meta data
+        self.level = 0
+        self.name = ""
+        self.classType = ""
+
+        # Meters
+        self.health = 0
+        self.magic = 0
+        self.mechanical = 0
+        self.defense = 0
+        self.magicDefense = 0
+
+        # Inventory
+        self.loot = []
+        self.food = []
+
+    def PlotRisk(self, attacks: list) -> None:
+        """
+        Goes through enemeis potential attacks and damage according to our defense profile
+        Prints how risk each attack could be
+        :param attacks: List containing attack data [name, mechanical damage, magic damage, health damage]
+        """
+        risk_levels = {
+            "high": "is a high risk attack",
+            "medium": "is a medium risk attack",
+            "low": "is a low risk attack",
+            "none": "is a no risk attack",
+        }
+
+        for attack in attacks:
+            name, mech, magic, health = attack
+            print(f"{name} ", end="")
+            if self.mechanical <= mech:
+                # attack stuns opponent ?
+                print(risk_levels["high"])
+                continue
+
+            total_damage = max(0, magic - self.magicDefense) + max(
+                0, health - self.defense
+            )
+            if total_damage >= self.health:
+                print(risk_levels["high"])
+            elif total_damage > self.health / 2:
+                print(risk_levels["medium"])
+            elif total_damage > self.health / 4:
+                print(risk_levels["low"])
+            else:
+                print(risk_levels["none"])
+
+
 class CharacterFactory:
     """
     A class for creating characters
@@ -74,55 +126,3 @@ class CharacterFactory:
         character.food = data["food"]
 
         return character
-
-
-class Character:
-    def __init__(self):
-        # Meta data
-        self.level = 0
-        self.name = ""
-        self.classType = ""
-
-        # Meters
-        self.health = 0
-        self.magic = 0
-        self.mechanical = 0
-        self.defense = 0
-        self.magicDefense = 0
-
-        # Inventory
-        self.loot = []
-        self.food = []
-
-    def PlotRisk(self, attacks: list) -> None:
-        """
-        Goes through enemeis potential attacks and damage according to our defense profile
-        Prints how risk each attack could be
-        :param attacks: List containing attack data [name, mechanical damage, magic damage, health damage]
-        """
-        risk_levels = {
-            "high": "is a high risk attack",
-            "medium": "is a medium risk attack",
-            "low": "is a low risk attack",
-            "none": "is a no risk attack",
-        }
-
-        for attack in attacks:
-            name, mech, magic, health = attack
-            print(f"{name} ", end="")
-            if self.mechanical <= mech:
-                # attack stuns opponent ?
-                print(risk_levels["high"])
-                continue
-
-            total_damage = max(0, magic - self.magicDefense) + max(
-                0, health - self.defense
-            )
-            if total_damage >= self.health:
-                print(risk_levels["high"])
-            elif total_damage > self.health / 2:
-                print(risk_levels["medium"])
-            elif total_damage > self.health / 4:
-                print(risk_levels["low"])
-            else:
-                print(risk_levels["none"])
