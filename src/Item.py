ec459d30bc3e9ea028560bc16f5bfa5fd6ffa000
Noah <noahbart742@gmail.com>
2024-04-02 15:57:27 -0500

    Added Item class

---
+++ src/Item.py
@@ -0,0 +1,39 @@
+
+images = {
+    "sword": "assets/sword.png",
+    "spear": "assets/spear.png",
+}
+
+WeaponStats = {
+    "sword": [20,30,10]
+}
+
+FoodStats = {}
+
+ArmorStats = {}
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+        self.image = images[self.name]
+        self.stats = None
+
+    def __str__(self) -> str:
+        return self.name, self.stats
+        
+        
+class Weapon(Item):
+    def __init__(self, name):
+        super().__init__(name)
+        self.stats = WeaponStats[name]
+
+class Food(Item):
+    def __init__(self, name):
+        super().__init__(name)
+        self.stats = FoodStats[name]
+
+class Armor(Item):
+    def __init__(self, name):
+        super().__init__(name)
+        self.stats = ArmorStats[name]
