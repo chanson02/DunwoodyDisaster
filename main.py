6f72b35b871726c77700eada0347d2fb6d56c4c1
Cooper Hanson <cooperhanson2020@gmail.com>
2024-04-12 14:48:28 -0500

    Moved the stack widget into the `main` file

--- main.py
+++ main.py
@@ -3,17 +3,33 @@ The entry point for the game
 """
 
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from dunwoody_disaster.views.fightScreen import FightScreen
 
+from StartMenuTest import StartMenu
+from MapScreen import MapScreen  # Assuming MapScreen.py contains MapScreen class
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Pokemon-like Game")
-        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("Dunwoody-Disaster")
+        # self.setGeometry(100, 100, 800, 600)
 
-        self.setCentralWidget(FightScreen())
+        self.startMenu = StartMenu()
+        self.startMenu.startButton.clicked.connect(self.showMapScreen)
+        self.mapScreen = MapScreen()
+        self.fightScreen = FightScreen()
+
+        self.stack = QStackedWidget()
+        self.stack.addWidget(self.startMenu)
+        self.stack.addWidget(self.mapScreen)
+        self.stack.addWidget(self.fightScreen)
+
+        # Set the stacked widget as the central widget of the main window
+        self.setCentralWidget(self.stack)
+
+    def showMapScreen(self):
+        self.stack.setCurrentIndex(1)
 
 
 if __name__ == "__main__":
