a0e627b46986c7ebe3254d4fbdc3c029173a9d4f
Cooper Hanson <cooperhanson2020@gmail.com>
2024-04-18 18:07:49 -0500

    Make victory screen also use the unimplemented callback

--- dunwoody_disaster/views/victoryScreen.py
+++ dunwoody_disaster/views/victoryScreen.py
@@ -1,12 +1,15 @@
+from typing import Callable
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PySide6.QtCore import Qt
 from dunwoody_disaster.FightSequence import FightSequence
 from dunwoody_disaster.views.collectLootScreen import CollectLootScreen
+import dunwoody_disaster as DD
 
 
 class VictoryScreen(QWidget):
     def __init__(self, fight_controller: FightSequence):
         super().__init__()
+        self._callback = DD.unimplemented
 
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
@@ -23,6 +26,7 @@ class VictoryScreen(QWidget):
         layout.addWidget(pic)
 
         lbl = QLabel("Manage your inventory: ")
+        lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(lbl)
 
         loot_screen = CollectLootScreen(
@@ -31,6 +35,9 @@ class VictoryScreen(QWidget):
         loot_screen.set_callback(self.loot_selected_event)
         layout.addWidget(loot_screen)
 
+    def set_callback(self, callback: Callable):
+        self._callback = callback
+
     def loot_selected_event(self):
-        print("Player selected the loot they wanted")
-        # TODO: Return to map
+        self.deleteLater()
+        self._callback()
