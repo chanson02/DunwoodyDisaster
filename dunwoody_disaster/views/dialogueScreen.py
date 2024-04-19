ef69950f75d29efc66e063d86b86e9f0092c5029
Cooper Hanson <cooperhanson2020@gmail.com>
2024-04-19 14:47:08 -0500

    Can cycle through dialogues by clicking

--- dunwoody_disaster/views/dialogueScreen.py
+++ dunwoody_disaster/views/dialogueScreen.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QStackedLayout, QVBoxLayout, QLabel, QHBoxLayout, QGroupBox
 from dunwoody_disaster.CharacterFactory import Character
+import dunwoody_disaster as DD
 
 
 class DialogueScreen(QWidget):
@@ -10,6 +11,7 @@ class DialogueScreen(QWidget):
         :param char2: The character to show on the right side
         """
         super().__init__()
+        self._index = 0
         self.char1 = char1
         self.char2 = char2
 
@@ -24,10 +26,13 @@ class DialogueScreen(QWidget):
         self.char2_dialogue = QLabel("")
 
         self.init_ui()
+        DD.clickable(self).connect(self.next_dialogue)
 
     def set_dialogue(self, char1: list[str], char2: list[str]):
         self._char1_dialogue = char1
         self._char2_dialogue = char2
+        self._index = 0
+        self.next_dialogue()
 
     def init_ui(self):
         layout = QVBoxLayout()
@@ -54,3 +59,18 @@ class DialogueScreen(QWidget):
         container.addWidget(self.char2_dialogue)
         player2_dialogue_box.setLayout(container)
         self.dialogue_stack.addWidget(player2_dialogue_box)
+
+        #self.next_dialogue()
+
+    def next_dialogue(self):
+        index = self._index // 2
+        char = self._index % 2
+
+        if char == 0:
+            self.char1_dialogue.setText(self._char1_dialogue[index])
+        else:
+            self.char2_dialogue.setText(self._char2_dialogue[index])
+
+        self.dialogue_stack.setCurrentIndex(char)
+        self._index += 1
+        return
