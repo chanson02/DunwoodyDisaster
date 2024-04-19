3c9d3eef60ea40f3379392f773b135e9c6bd30d8
Noah <noahbart742@gmail.com>
2024-04-19 15:12:54 -0500

    Formatting

--- dunwoody_disaster/views/defeatScreen.py
+++ dunwoody_disaster/views/defeatScreen.py
@@ -19,6 +19,7 @@ styles = {
     "return-button": "border-radius: 15px; height: 42px; background-color: white; color: black; font-size: 24px; width: 200px;",
 }
 
+
 class DefeatScreen(QWidget):
     def __init__(self, image: str = None):
         """
@@ -28,7 +29,7 @@ class DefeatScreen(QWidget):
         super().__init__()
 
         self.show = True
-        
+
         self.image = image
 
         layout = QVBoxLayout()
@@ -37,37 +38,35 @@ class DefeatScreen(QWidget):
         container = QVBoxLayout()
         container.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        #UI Elements
-        #Game over label
+        # UI Elements
+        # Game over label
         defeat_label = QLabel("You have been defeated")
         defeat_label.setStyleSheet(styles["title-text"] + styles["center"])
 
-        #Return Button
+        # Return Button
         return_button = QPushButton("Return to Map")
         return_button.setStyleSheet(styles["return-button"] + styles["center"])
         return_button.clicked.connect(self.return_to_map)
 
-        #Add defeat Image
+        # Add defeat Image
         if self.image is not None:
             defeat_image = QPixmap(self.image)
             Imagelabel = QLabel(self)
             Imagelabel.setPixmap(defeat_image)
             container.addWidget(Imagelabel)
 
-        
         container.addWidget(defeat_label)
         container.addWidget(return_button)
         layout.addLayout(container)
         self.setLayout(layout)
 
-
-    #Functions
+    # Functions
     def return_to_map(self):
         """
         Returns the player to the map screen
 
         """
-        self.show = False 
+        self.show = False
 
     def SetImage(self, image: str):
         """
@@ -76,6 +75,7 @@ class DefeatScreen(QWidget):
         """
         self.image = QPixmap(image)
         self.label.setPixmap(self.image)
+
 
 # For testing purposes
 # class MainWindow(QMainWindow):
