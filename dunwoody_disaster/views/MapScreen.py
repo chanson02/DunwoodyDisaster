331362c6e5470dfe63da412204180ab8935521a5
Cooper Hanson <cooperhanson2020@gmail.com>
2024-04-19 21:35:20 -0500

    Added a way to draw the map without placing a character

--- dunwoody_disaster/views/MapScreen.py
+++ dunwoody_disaster/views/MapScreen.py
@@ -7,18 +7,22 @@ from PySide6.QtWidgets import QGridLayout, QWidget, QLabel
 from PySide6.QtGui import QPixmap, QKeyEvent, QPainter, QMouseEvent
 
 import dunwoody_disaster as DD
+from dunwoody_disaster.CharacterFactory import Character
+
 
 class MapScreen(QWidget):
-    def __init__(self, character):
+    def __init__(self, character: Character):
         super().__init__()
+        self.character = character
+
         self.mainLayout = QGridLayout()
         self.mainLayout.setSpacing(0)
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.mainLayout)
 
         self.map = QLabel()
-        self.map.setPixmap(self.pixmap())
         self.map.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        self.move_character(-1, 0)
         self.mainLayout.addWidget(self.map)
 
     def keyPressEvent(self, event: QKeyEvent):
@@ -34,14 +38,17 @@ class MapScreen(QWidget):
             print("right")
 
     def move_character(self, x: int, y: int):
+        if x < 0:
+            self.map.setPixmap(self.pixmap())
+            return
+
         map_pixmap = self.pixmap()
         painter = QPainter(map_pixmap)
-        overlay = QPixmap(DD.ASSETS['cooper']).scaledToWidth(80)
+        overlay = QPixmap(self.character.image()).scaledToWidth(80)
         painter.drawPixmap(x, y, overlay)
         painter.end()
         self.map.setPixmap(map_pixmap)
         self.repaint()
-
 
     def mousePressEvent(self, event: QMouseEvent):
         point = event.pos()
@@ -49,4 +56,4 @@ class MapScreen(QWidget):
         self.move_character(point.x(), point.y())
 
     def pixmap(self):
-        return QPixmap(DD.ASSETS['MainMap_Coop_bus'])
+        return QPixmap(DD.ASSETS["MainMap_Coop_bus"])
