852933c9c7ca978b260ae92d515b1d69ae2de1f7
John Vue <johncollegeacc@gmail.com>
2024-04-30 15:15:26 -0500

    Got Animations to show on the background battle scene

--- dunwoody_disaster/animations/idle.py
+++ dunwoody_disaster/animations/idle.py
@@ -1,26 +1,44 @@
 import pygame
 from dunwoody_disaster.animations.PygameAnimation import PygameAnimation
 import dunwoody_disaster as DD
-#from typing import override
+from dunwoody_disaster import ASSETS
+
+# from typing import override
 
 
 class IdleAnimation(PygameAnimation):
-    def __init__(self):
+    def __init__(
+        self,  # animation_path: str, frame_count: int = 8, frame_duration: int = 100#
+    ):
         super().__init__()
+        """         self.animation_base_path = animation_path
+        self.frame_count = frame_count
+        self.frame_duration = frame_duration """
         self.load_frames()
 
-    #@override
+    # @override
     def load_frames(self):
         self.frames = []
         self.frame_count = 8
         self.frame_duration = 100
         animation_base_path = f"{DD.ANIMATION_PATH}/Idle"
+        self.background_img = pygame.image.load(ASSETS["background"]).convert()
 
         for i in range(self.frame_count):
             path = f"{animation_base_path}_{str(i + 1).zfill(2)}.png"
             self.frames.append(pygame.image.load(path).convert_alpha())
 
-    #@override
+    # @override
     def run(self) -> None:
         if self.running and self.should_render():
-            self.surface.blit(self.next_frame(), (350, 250))
+            self.surface.blit(
+                pygame.transform.scale(self.background_img, self.surface.get_size()),
+                (0, 0),
+            )
+            self.surface.blit(self.next_frame(False), (350, 250))
+
+    def draw(self, screen, position, dimensions):
+        """Draw the current frame of the animation on the provided screen at the specified position."""
+        frame = self.current_frame()
+        frame = pygame.transform.scale(frame, dimensions)
+        screen.blit(frame, position)
