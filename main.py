faf8a1cd18c4280dba08b5b9125ede0fe47ff897
Mitchell Humphries <mitchgt@comcast.net>
2024-05-01 18:14:54 -0500

    Format V2
    -Fix for lint

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from dunwoody_disaster.views.StartMenu import StartMenu
 from dunwoody_disaster.views.MapScreen import MapScreen, Map
 from dunwoody_disaster.views.CharacterSelector import CharacterSelector
 from dunwoody_disaster.CharacterFactory import CharacterFactory, Character
-import dunwoody_disaster as DD
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Dunwoody-Disaster")
-        self.setStyleSheet("background-color: #2f2f2f;")
+        self.setStyleSheet("background-color: #2f2f2f; color: #FFFFFF")
         self.player = None
 
         self.startMenu = StartMenu()
