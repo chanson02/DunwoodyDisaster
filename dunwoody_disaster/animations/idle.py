9921c0102fb638cb24b668202a21d4ecc8dc82ab
Cooper Hanson <cooperhanson2020@gmail.com>
2024-05-06 16:15:00 -0500

    Removed some unused stuff

--- dunwoody_disaster/animations/idle.py
+++ dunwoody_disaster/animations/idle.py
@@ -1,34 +1,3 @@
-# import pygame
-# import math
-# from dunwoody_disaster.animations.PygameAnimation import PygameAnimation
-# import dunwoody_disaster as DD
-#
-#
-# class IdleAnimation(PygameAnimation):
-#     def __init__(self, background: str, player: str, enemy: str):
-#         super().__init__()
-#         self.bkg = pygame.image.load(background).convert_alpha()  # 666x360 image
-#         self.player = pygame.transform.scale(pygame.image.load(player), (100, 100))
-#         self.enemy = pygame.transform.scale(pygame.image.load(enemy), (100, 100))
-#
-#         self.bob_amplitude = 10
-#         self.bob_frequency = 0.1
-#         self.elapsed = 0
-#
-#     @DD.override
-#     def run(self) -> None:
-#         if self.running:
-#             self.draw()
-#             self.clock.tick(20)
-#             self.elapsed += 1
-#
-#     def draw(self):
-#         self.surface.blit(self.bkg, (7, 0))
-#         factor = self.elapsed * self.bob_frequency + 0.5
-#         offset = int(self.bob_amplitude * math.sin(factor))
-#         self.surface.blit(self.player, (100, 200 + offset))
-#         self.surface.blit(self.enemy, (450, 200 - offset))
-
 import pygame
 import math
 from dunwoody_disaster.animations.AnimationComponent import AnimationComponent
