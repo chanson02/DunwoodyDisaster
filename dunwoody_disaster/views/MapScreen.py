b10268233109887086a0cba1b082434d86ad40e8
EngineeringDude123 <johncollegeacc@gmail.com>
2024-05-09 16:44:55 -0500

    Added Arena maps

--- dunwoody_disaster/views/MapScreen.py
+++ dunwoody_disaster/views/MapScreen.py
@@ -32,8 +32,6 @@ class Map(QLabel):
         else:
             self.char_pos = (-1, 0)
 
-
-
     def onRoomChange(self, callback: Callable):
         self.roomChanged = callback
 
@@ -115,13 +113,19 @@ class Map(QLabel):
         chars = CharacterFactory
         map = Map(char)
         map.setAsset("MainMap")
-        map.addRoom("Bus Stop", (419, 700), chars.JoeAxberg(), "MathClass+")
-        map.addRoom("Court Yard", (693, 559), chars.LeAnnSimonson(), "Library+")
-        map.addRoom("Commons", (451, 449), chars.RyanRengo(), "ScienceClass+")
-        map.addRoom("Math", (236, 359), chars.NoureenSajid(), "Courtyard+")
-        map.addRoom("English", (770, 366), chars.AmalanPulendran(), "ComputerLab+")
-        map.addRoom("Science", (490, 217), chars.MatthewBeckler(), "MathClass+")
-        map.addRoom("Dean's Office", (90, 589), chars.BillHudson(), "DeansOffice+")
+        map.addRoom("Bus Stop", (419, 700), chars.JoeAxberg(), "MathClassResized")
+        map.addRoom("Court Yard", (693, 559), chars.LeAnnSimonson(), "LibraryResized")
+        map.addRoom("Commons", (451, 449), chars.RyanRengo(), "ScienceClassResized")
+        map.addRoom("Math", (236, 359), chars.NoureenSajid(), "CourtyardResized")
+
+        map.addRoom(
+            "English", (770, 366), chars.AmalanPulendran(), "ComputerLabResized"
+        )
+
+        map.addRoom("Science", (490, 217), chars.MatthewBeckler(), "MathClassResized")
+        map.addRoom(
+            "Dean's Office", (90, 589), chars.BillHudson(), "DeansOfficeResized"
+        )
         return map
 
     def serialize(self) -> dict:
