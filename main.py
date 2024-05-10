a0100886ea11a02458e21d33383574f8bbc5b425
Mitchell Humphries <mitchgt@comcast.net>
2024-05-10 16:13:42 -0500

    Format V1

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ class MainWindow(QMainWindow):
 
         self.stack = QStackedWidget()
         self.stack.addWidget(self.startMenu)
-        
 
         # Set the stacked widget as the central widget of the main window
         self.setCentralWidget(self.stack)
