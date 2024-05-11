487db160b75db98d3a00b77265c347c28cefd4f2
Cooper Hanson <cooperhanson2020@gmail.com>
2024-05-10 20:32:24 -0500

    Got the QMovie to resize

--- dunwoody_disaster/views/StartMenu.py
+++ dunwoody_disaster/views/StartMenu.py
@@ -2,8 +2,8 @@ import pygame
 
 from typing import Callable
 
-from PySide6.QtGui import QMovie
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QMovie, QPainter
+from PySide6.QtCore import Qt, QSize, QRect
 from PySide6.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
@@ -12,9 +12,95 @@ from PySide6.QtWidgets import (
     QPushButton,
     QWidget,
     QApplication,
+    QSizePolicy
 )
 
 import dunwoody_disaster as DD
+
+
+# Got this from https://stackoverflow.com/questions/77602181/pyside6-how-do-i-resize-a-qlabel-playing-a-qmovie-and-maintain-the-movies-orig
+class MovieLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self._movieSize = QSize()
+        self._minSize = QSize()
+
+    def minimumSizeHint(self):
+        if self._minSize.isValid():
+            return self._minSize
+        return super().minimumSizeHint()
+
+    def setMovie(self, movie: QMovie):
+        if self.movie() == movie:
+            return
+        super().setMovie(movie)
+
+        if not movie.isValid():
+            self._movieSize = QSize()
+            self._minSize = QSize()
+            self.updateGeometry()
+            return
+
+        cf = movie.currentFrameNumber()
+        state = movie.state()
+        movie.jumpToFrame(0)
+        rect = QRect()
+        for i in range(movie.frameCount()):
+            movie.jumpToNextFrame()
+            rect |= movie.frameRect()
+
+        width = rect.x() + rect.width()
+        height = rect.y() + rect.height()
+
+        self._movieSize = QSize(width, height)
+        if width == height and False:
+            if width < 4:
+                self._minSize = QSize(width, width)
+            else:
+                self._minSize = QSize(4, 4)
+        else:
+            minimum = min(width, height)
+            maximum = max(width, height)
+            ratio = maximum / minimum
+            base = min(4, minimum)
+            self._minSize = QSize(base, round(base * ratio))
+            if minimum == width:
+                self._minSize.transpose()
+
+        movie.jumpToFrame(cf)
+        if state == movie.MovieState.Running:
+            movie.setPaused(False)
+        self.updateGeometry()
+
+    def paintEvent(self, event):
+        movie = self.movie()
+        if not isinstance(movie, QMovie) or not movie.isValid():
+            super().paintEvent(event)
+            return
+
+        qp = QPainter(self)
+        self.drawFrame(qp)
+
+        cr = self.contentsRect()
+        margin = self.margin()
+        cr.adjust(margin, margin, -margin, -margin)
+
+        style = self.style()
+        alignment = style.visualAlignment(self.layoutDirection(), self.alignment())
+        maybeSize = self._movieSize.scaled(cr.size(), Qt.AspectRatioMode.KeepAspectRatio)
+
+        if maybeSize != movie.scaledSize():
+            movie.setScaledSize(maybeSize)
+            style.drawItemPixmap(
+                qp, cr, alignment, 
+                movie.currentPixmap().scaled(cr.size(), Qt.AspectRatioMode.KeepAspectRatio)
+            )
+
+        else:
+            style.drawItemPixmap(
+                qp, cr, alignment, 
+                movie.currentPixmap()
+            )
 
 class StartMenu(QWidget):
     def __init__(self):
@@ -25,13 +111,14 @@ class StartMenu(QWidget):
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
+        self.setStyleSheet('border: 1px solid green;')
 
-        bkg = QLabel("This is a test")
-        bkg.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        bkg.setStyleSheet('min-height: 500px; min-width: 500px;')
         self.movie = QMovie(DD.ASSETS["FinalTitle"])
         self.movie.start()
+
+        bkg = MovieLabel()
         bkg.setMovie(self.movie)
+        bkg.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(bkg)
 
         btns = QHBoxLayout()
